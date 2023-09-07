683aa7cbdbc1e018ad5a57cfdb21d1b9d5312e97
LIvanoff <drfrily@gmail.com>
2023-09-07 21:56:10 +0300

    update RadialBasis

--- function.py
+++ function.py
@@ -2,9 +2,5 @@ import torch
 
 
 class RadialBasis:
-    def __init__(self):
-        self.m = 1.
-        self.sigma = 1.
-
     def __call__(self, x, *args, **kwargs):
-        return torch.exp(torch.pow(torch.abs(self.m - x), 2) / 2 * self.sigma)
+        return torch.exp(-torch.pow(torch.abs(torch.mean(x) - x), 2) / 2 * torch.std(x))
